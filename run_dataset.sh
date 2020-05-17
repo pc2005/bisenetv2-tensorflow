f645049a0eaedaed5012cf1217d1e72edc04b7d0
pc2005 <philchen2005@gmail.com>
2020-05-17 15:32:46 -0400

    Updated for local test.

---
+++ run_dataset.sh
@@ -0,0 +1,1 @@
+python tools/evaluate_bisenetv2_cityscapes.py --pb_file_path ./checkpoint/bisenetv2_cityscapes_frozen.pb --dataset_dir ./data/pc_dataset/cityscapes
---
+++ run_single_img.sh
@@ -0,0 +1,1 @@
+python tools/test_bisenetv2_cityscapes.py --weights_path ./model/cityscapes/bisenetv2/cityscapes.ckpt --src_image_path ./data/test_image/test_01.png
